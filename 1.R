bebf2309f814dbd822b9775fba8ae48c1704fd8b
minoeru <minoeru@users.noreply.github.com>
2019-12-07 10:13:35 +0900

    first commit

---
+++ 1.R
@@ -0,0 +1,7 @@
+Reverse("stressed")
+
+Reverse <- function(tex){
+    num <- nchar(tex)
+    hoge <-lapply(1:num,function(x){substring(tex,num-x+1,num-x+1)})
+    return (paste(unlist(hoge),collapse=""))
+}
