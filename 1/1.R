1d8283ea0a4b8a8968fb4ebd9828de1a9385ca3a
minoeru <minoeru@users.noreply.github.com>
2019-12-09 10:55:33 +0900

    add 1

---
+++ 1/1.R
@@ -0,0 +1,8 @@
+Func1("パタトクカシーー")
+
+Func1 <- function(text){
+	num <- c(1,3,5,7)
+	hoge <- lapply(1:length(num),function(x){substring(text,num[x],num[x])})
+	return(paste(unlist(hoge),collapse=""))
+
+}
