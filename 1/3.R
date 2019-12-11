fed29d20450a701eb19a70f9ce023a8d91bc61b9
minoeru <minoeru@users.noreply.github.com>
2019-12-11 10:43:19 +0900

    add 3

---
+++ 1/3.R
@@ -0,0 +1,7 @@
+Func3("Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.")
+
+Func3 <- function(tex){
+  tex <- gsub("\\.|,","",tex)
+  hoge <- unlist(strsplit(tex," "))
+  nchar(fuga)
+}
