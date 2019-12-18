c606c6d2996bf6007f4eccc2b9739d829e1996fa
minoeru <minoeru@users.noreply.github.com>
2019-12-18 17:37:39 +0900

    add 9

---
+++ 1/9.R
@@ -0,0 +1,22 @@
+Func9("I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind.")
+
+Func9 <- function(tex){
+  hoge <- unlist(strsplit(text," "))
+  num <- length(hoge)
+  ans <- lapply(1:num,function(x){ifelse(nchar(hoge[x]) < 4,hoge[x],Change(hoge[x]) )})
+  ans <- paste(unlist(ans),collapse=" ")
+  return(ans)
+}
+
+Change <- function(tex){
+  top <- substring(tex,1,1)
+  end <- substring(tex,nchar(tex),nchar(tex))
+  center <- substring(tex,2,nchar(tex)-1)
+  fuga <- nchar(center)
+  piyo <- floor(runif(1000,1,fuga+1))
+  piyo <- unique(piyo)
+  hogera <- lapply(1:fuga,function(x){substring(center,piyo[x],piyo[x])})
+  hogera <- unlist(hogera)
+  hogera <- paste(hogera,collapse="")
+  return(paste0(top,hogera,end,collapse=""))
+}
